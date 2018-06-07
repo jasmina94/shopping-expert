fb31d324ce3b7f7ed08912a31c6d02b86576ba79
desanka <desanka.todic@engagis.com>
2018-06-07 20:09:56 +0200

    View for settings fixed.

--- MDJ/app/src/main/java/com/ftn/mdj/activities/MainActivity.java
+++ MDJ/app/src/main/java/com/ftn/mdj/activities/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -66,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         String tkn = FirebaseInstanceId.getInstance().getToken();
 
         mAuth = FirebaseAuth.getInstance();
@@ -99,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             }
             case R.id.mnu_settings: {
-                Intent intent = new Intent(MainActivity.this, SettingsBasicActivity.class);
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
                 break;
             }
--- MDJ/app/src/main/java/com/ftn/mdj/fragments/SettingsFragment.java
+++ MDJ/app/src/main/java/com/ftn/mdj/fragments/SettingsFragment.java
@@ -3,19 +3,24 @@ package com.ftn.mdj.fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Message;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.ftn.mdj.R;
 import com.ftn.mdj.activities.MainActivity;
+import com.ftn.mdj.activities.SettingsActivity;
 import com.ftn.mdj.threads.SettingsThread;
 import com.ftn.mdj.threads.UploadListThread;
 import com.ftn.mdj.utils.SharedPreferencesManager;
@@ -37,19 +42,11 @@ public class SettingsFragment extends PreferenceFragment {
         mContext = this.getActivity();
         mActivity = this.getActivity();
 
-        System.out.println("op1   "+SharedPreferencesManager.Key.SHOW_NOTIFICATIONS.name());
-
         Boolean showNotifications =  SharedPreferencesManager.getInstance(mContext).getBoolean(SharedPreferencesManager.Key.SHOW_NOTIFICATIONS.name());
-
         long userId = SharedPreferencesManager.getInstance(mContext).getInt(SharedPreferencesManager.Key.USER_ID.name());
 
-        System.out.println("op2   "+ showNotifications);
-
-        final SwitchPreference onOffNotifications = (SwitchPreference) findPreference("switch");
-
+        final SwitchPreference onOffNotifications = (SwitchPreference) findPreference("notifications_switch");
         onOffNotifications.setChecked(showNotifications);
-
-        // SwitchPreference preference change listener
         onOffNotifications.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
@@ -77,5 +74,71 @@ public class SettingsFragment extends PreferenceFragment {
                 return false;
             }
         });
+
+        /*if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DesaTheme);
+            Toast.makeText(getApplicationContext(),"DesaTheme", Toast.LENGTH_SHORT).show();
+        }else{
+            setTheme(R.style.AppTheme);
+            Toast.makeText(getApplicationContext(),"AppTheme", Toast.LENGTH_SHORT).show();
+        }*/
+
+        final SwitchPreference onOffDarkTheme = (SwitchPreference) findPreference("theme_switch");
+        onOffDarkTheme.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                if(onOffDarkTheme.isChecked()){
+                    Toast.makeText(mActivity,"Unchecked", Toast.LENGTH_SHORT).show();
+
+                    // Checked the switch programmatically
+                    onOffDarkTheme.setChecked(false);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+
+                }else {
+                    Toast.makeText(mActivity,"Checked",Toast.LENGTH_SHORT).show();
+
+                    // Unchecked the switch programmatically
+                    onOffDarkTheme.setChecked(true);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+
+                return false;
+            }
+        });
+
+        final EditTextPreference blockedEditText = (EditTextPreference) findPreference("blocked_text");
+
+        blockedEditText.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                String emailToBlock = blockedEditText.getEditText().getEditableText().toString();
+                System.out.print("blockedEditText " + emailToBlock);
+                Toast.makeText(mActivity,"edit "+emailToBlock , Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        final ListPreference blockedList = (ListPreference) findPreference("blocked_list");
+        CharSequence[] entries = new CharSequence[]{"One", "Two", "Three"};
+        blockedList.setEntries(entries);
+        blockedList.setEntryValues(entries);
+
+        blockedList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                String emailToUnblock = o.toString();
+                System.out.print("desaaa " + emailToUnblock);
+                Toast.makeText(mActivity,"edit "+emailToUnblock , Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    public void restartApp(){
+        Intent i = new Intent(mContext,MainActivity.class);
+        startActivity(i);
+        mActivity.finish();
     }
 }
