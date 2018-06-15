07a7018da13f452330a39cc0f25fa1a773d25252
desanka <desanka.todic@engagis.com>
2018-06-15 21:36:50 +0200

    Uploaded demo.

--- MDJ/app/src/main/java/com/ftn/mdj/utils/MyNotification.java
+++ MDJ/app/src/main/java/com/ftn/mdj/utils/MyNotification.java
@@ -1,13 +1,16 @@
 package com.ftn.mdj.utils;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.ftn.mdj.R;
@@ -20,6 +23,18 @@ public class MyNotification {
 
         System.out.println("Primio poruku ===============================================================");
 
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("M_CH_ID", "Location Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, "M_CH_ID")
                 .setSmallIcon(R.drawable.logo)
@@ -27,6 +42,7 @@ public class MyNotification {
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.logo))
                 .setContentText(body)
                 .setAutoCancel(true)
+                .setSmallIcon(R.drawable.logo)
                 .setVibrate(new long[]{100, 100, 100, 100})
                 .setPriority(android.app.Notification.PRIORITY_MAX)
                 .setSound(defaultSoundUri);
@@ -44,10 +60,12 @@ public class MyNotification {
 
 
         notificationBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        NotificationManager mNotificationManager =
+//                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
 
-        mNotificationManager.notify(1, notificationBuilder.build());
+//        mNotificationManager.notify(1, notificationBuilder.build());
+        notificationManager.notify(1, notificationBuilder.build());
+
     }
 }
